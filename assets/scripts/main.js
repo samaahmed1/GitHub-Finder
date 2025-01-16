08ceecc66384ecb677b1ffc8e0a1297f6a097f2c
samaahmed1 <sama34777@gmail.com>
2025-01-16 16:39:36 +0200

    Fetch GitHub User Repos and Display it Dynamically

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -23,7 +23,23 @@ function getUserData() {
         <h1>${data.name}</h1>
         <a href= "${data.html_url}" target="_blank">GitHub</a>
         </div>
+        <ol id= "user-repos" class= "user-repos container"></ol>
         `;
+      });
+    fetch(`https://api.github.com/users/${document.getElementById("input-search").value}/repos`)
+      .then((response) => response.json())
+      .then((data) => {
+        let repos = "";
+        for (let i = 0; i < data.length; i++) {
+          repos += `
+        <li>
+        <a href="${data[i].homepage}" target="_blank">
+          ${data[i].name}
+        </a>
+      </li>
+        `;
+        }
+        document.getElementById("user-repos").innerHTML = repos;
       });
   }
 }
